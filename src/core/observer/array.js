2c19c370b7a8ae4c85f58707e9ae89eefe27c61e
沈军 <shenjun@qccr.com>
2023-03-13 14:25:45 +0800

    feat [add vue arrayhooks]

--- src/core/observer/array.js
+++ src/core/observer/array.js
@@ -6,6 +6,7 @@
 import { def } from '../util/index'
 
 const arrayProto = Array.prototype
+// 创建 arrayMethods 对象，它的原型指向 arrayProto
 export const arrayMethods = Object.create(arrayProto)
 
 const methodsToPatch = [
@@ -24,6 +25,7 @@ const methodsToPatch = [
 methodsToPatch.forEach(function (method) {
   // cache original method
   const original = arrayProto[method]
+  // 重写 arrayMethods 对象的 7 个方法
   def(arrayMethods, method, function mutator (...args) {
     const result = original.apply(this, args)
     const ob = this.__ob__
@@ -37,8 +39,10 @@ methodsToPatch.forEach(function (method) {
         inserted = args.slice(2)
         break
     }
+    // inserted 表示生成了新的元素，需要对新元素进行 observe
     if (inserted) ob.observeArray(inserted)
     // notify change
+    // 通知依赖更新
     ob.dep.notify()
     return result
   })
